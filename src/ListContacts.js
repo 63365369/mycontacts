f3087ea3aea46bee980dad0ebd827064e026106e
63365369 <63365369@qq.com>
2017-11-17 14:20:19 +0800

    去除冗余
    去除冗余

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,8 +1,7 @@
 import React,{Component} from 'react';
+
 class ListContacts extends Component{
     render() {
-
-      console.log('props',this.props);
         return (
             <ol className='contact-list'>
                 {this.props.contacts.map((contact)=>(
